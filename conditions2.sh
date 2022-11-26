b0c2a4e4b45eeb24c3dcc340e13eba41b00173cb
AdeepaAdithya <119093312+AdeepaAdithya@users.noreply.github.com>
2022-11-26 12:37:52 +0530

    Delete conditions2.sh

--- conditions2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#user_input_with_conditions.
-echo "what's your name?"
-read a
-if [ "$a" = "AAG" ]; then
-  echo "This is my name. It's true"
-else
-  echo "this isn't my name.retry."
-fi
